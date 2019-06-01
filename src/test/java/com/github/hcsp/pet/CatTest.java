218f3c36431f069a991ef1d50686fbbeda605503
Bo Zhang <bo@gradle.com>
2019-06-01 17:18:31 +0800

    Use CaptureSystemOutputExtension

--- src/test/java/com/github/hcsp/pet/CatTest.java
+++ src/test/java/com/github/hcsp/pet/CatTest.java
@@ -1,19 +1,17 @@
 package com.github.hcsp.pet;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.Matchers.containsStringIgnoringCase;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import com.github.blindpirate.extensions.CaptureSystemOutput;
 import org.junit.jupiter.api.Test;
 
 public class CatTest {
     @Test
-    public void catCanSayItsName() {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(baos, true));
+    @CaptureSystemOutput
+    public void catCanSayItsName(CaptureSystemOutput.OutputCapture capture) {
+        capture.expect(containsStringIgnoringCase("abcdef"));
         Cat cat = new Cat();
         cat.name = "ABCDEF";
         cat.sayName();
-        assertTrue(new String(baos.toByteArray()).toLowerCase().contains("abcdef"));
     }
 }
