d2a4c7ce1105c18e32d1f934da112484641c8ce9
liboyang <huagongyilby@126.com>
2019-06-05 09:41:26 +0800

    创建一个一个实例方法

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -1,10 +1,13 @@
 package com.github.hcsp.pet;
 
+import sun.awt.geom.AreaOp;
+
 public class Cat {
     public String name;
 
     public void sayName() {
         // Print itself's name to standard output (System.out) here
         // 在这里将自己的名字打印到标准输出（System.out）
+        System.out.println(name);
     }
 }
