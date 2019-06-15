0391e5b72d94b034e742d89cb89eb820db150673
Bo Zhang <bo@gradle.com>
2019-06-15 19:25:10 +0800

    Update repo

--- src/test/java/com/github/hcsp/pet/CatTest.java
+++ src/test/java/com/github/hcsp/pet/CatTest.java
@@ -1,13 +1,14 @@
 package com.github.hcsp.pet;
 
 import com.github.blindpirate.extensions.CaptureSystemOutput;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 public class CatTest {
     @Test
     @CaptureSystemOutput
     public void catCanSayItsName(CaptureSystemOutput.OutputCapture capture) {
-        capture.expect(containsStringIgnoringCase("abcdef"));
+        capture.expect(Matchers.containsStringIgnoringCase("abcdef"));
         Cat cat = new Cat();
         cat.name = "ABCDEF";
         cat.sayName();
