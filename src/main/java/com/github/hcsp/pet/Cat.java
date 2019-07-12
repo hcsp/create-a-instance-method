39e35eb9ccb46492cd9978360e5d0c6625249992
frank-0908 <2059326740@qq.com>
2019-07-12 21:19:10 +0800

    创建一个实例方法

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -4,8 +4,7 @@ public class Cat {
     public String name;
 
     public void sayName() {
-        name="费永敬";
-        System.out.println(name);
+        System.out.println("费永敬");
         // Print itself's name to standard output (System.out) here
         // 在这里将自己的名字打印到标准输出（System.out）
     }
