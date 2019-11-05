9f1d86ab22c920b110bf5574502a263fe41cdd3f
liuxing <1151879967@qq.com>
2019-11-05 21:00:42 +0800

    已补全实例方法

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -4,8 +4,8 @@ public class Cat {
     public String name;
 
     public void sayName() {
-        System.out.println("Black");
-        System.out.println("White");
+        System.out.println("我叫"+name);
+
         // Print itself's name to standard output (System.out) here
         // 在这里将自己的名字打印到标准输出（System.out）
     }
