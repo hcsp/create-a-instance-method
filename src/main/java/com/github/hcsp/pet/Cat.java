727923ff664e9d9ea9467a408fd94d08f19b48b3
lynnyanggod <ouyangyly@gmail.com>
2021-07-13 20:00:45 +0800

    added a sout to print its own name.

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -4,7 +4,6 @@ public class Cat {
     public String name;
 
     public void sayName() {
-        // Print itself's name to standard output (System.out) here
-        // 在这里将自己的名字打印到标准输出（System.out）
+        System.out.println(name);
     }
 }
