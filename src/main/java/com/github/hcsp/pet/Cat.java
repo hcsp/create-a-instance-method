d3c171f1a70d699ecb4176d63490cc354ec53c10
soohea <77014206+soohea@users.noreply.github.com>
2021-09-02 20:34:36 +0800

    创建一个实例方法 (#270)
    创建一个实例方法 (#270)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -1,10 +1,18 @@
 package com.github.hcsp.pet;
 
+import com.github.hcsp.Home;
+
 public class Cat {
     public String name;
 
     public void sayName() {
         // Print itself's name to standard output (System.out) here
         // 在这里将自己的名字打印到标准输出（System.out）
+        if (name != null) {
+            System.out.println(name);
+        } else {
+            System.out.println("没有名字");
+        }
+
     }
 }
